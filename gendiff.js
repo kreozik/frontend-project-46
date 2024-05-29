92e518c933a8c123469f6d60883544542b7778a6
kreozik <leonid280505@gmail.com>
2024-05-29 12:49:39 +0300

    1s

---
+++ gendiff.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+const hello = (name) => {
+  return `Hello, ${name}!`;
+};
+
+const command = (names, lower, upper) => {
+  for (const name of names) {
+    const msg = hello(name);
+    if (lower) {
+      console.log(msg.toLowerCase());
+    } else if (upper) {
+      console.log(msg.toUpperCase());
+    } else {
+      console.log(msg);
+    }
+  }
+};
+
+program
+  .option('-l, --lower', 'only use lowercase letters')
+  .option('-u, --upper', 'only use uppercase letters')
+  .parse(process.argv);
+
+const { args } = program;
+const options = program.opts();
+const { lower, upper } = options;
+
+command(args, lower, upper);
+
