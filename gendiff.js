e4074bc511f3e1b0749e384ebf9e3b4ba4b836d2
kreozik <155053189+kreozik@users.noreply.github.com>
2024-05-29 13:16:56 +0300

    1,5 stag

--- gendiff.js
+++ gendiff.js
@@ -7,14 +7,14 @@ program
 .name('gendiff')
 .description('Compares two configuration files and shows a difference.')
 .version('0.1.0', '-V, --version', 'output the version number')
-.helpOption('-h, --help', 'output usage information') 
-// .arguments('<options')
+.helpOption('-h, --help', 'output usage information')
+.option('-f, --format [type]', 'output format') 
+.arguments('<filepath1> <filepath2>')
 .action((filepath1, filepath2, options) => {
   console.log(genDiff(filepath1, filepath2, options.format));
 });
+
+
+
 program.parse();
-const { args } = program;
-
-command(args);
-
 
